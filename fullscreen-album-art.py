c921d56c1917b830fbd89618789bef0ae627ebfb
Matthew Petela <matthew@petela.dev>
2020-10-14 01:14:07 -0400

    Image correctly centered and scaled

--- fullscreen-album-art.py
+++ fullscreen-album-art.py
@@ -16,18 +16,31 @@ def close(event):
 def spotify_setup():
     return None
 
+def resize_image(img, root):
+    min_size = min(root.winfo_screenheight(), root.winfo_screenwidth())
+    return img.resize((min_size, min_size))
+
+def center_image(img, root):
+    width = (root.winfo_screenwidth() - img.width) // 2
+    height = (root.winfo_screenheight() - img.height) // 2
+    return width, height
+
 def display_image(root):
     canvas = tk.Canvas(root)
-    canvas.pack(expand=True, fill='both')
-    img = ImageTk.PhotoImage(Image.open("21.jpg")) #get image
-    canvas.create_image(root.winfo_screenheight() // 2,root.winfo_screenwidth() // 2,anchor='center', image=img)
+    canvas.config(background="black") # change background to black
+    canvas.pack(expand=True, fill='both') # make canvas entire tk
+    img = Image.open("21.jpg") # get image
+    img = resize_image(img, root) if img.height != root.winfo_screenheight() and img.width != root.winfo_screenwidth() else img
+    width, height = center_image(img, root)
+    img = ImageTk.PhotoImage(img)
+    canvas.create_image(width, height, image=img, anchor='nw')
     root.bind('<Escape>', close)
     root.mainloop()
 
 
 def main():
     root = tkinter_init()
-    print(f"{root.winfo_screenheight()}x{root.winfo_screenwidth()}")
+    #print(f"{root.winfo_screenheight()}x{root.winfo_screenwidth()}")
     display_image(root)
     
 
